2d31aae56173f873fad5102f2813bc354c3427dd
Fiqry919 <muhammadfiqry919@gmail.com>
2025-01-18 08:20:07 +0700

    refactor(components): fix sidebar-opt

--- src/components/sidebar-opt.tsx
+++ src/components/sidebar-opt.tsx
@@ -5,6 +5,10 @@ import { SquareArrowOutUpRight } from "lucide-react"
 import { buttonVariants } from "@/components/ui/button"
 
 import {
+    SidebarMenu,
+    SidebarMenuItem
+} from "@/components/ui/sidebar"
+import {
     Card,
     CardContent,
     CardDescription,
@@ -14,27 +18,31 @@ import {
 
 export function SidebarOpt() {
     return (
-        <Card className="shadow-none">
-            <form>
-                <CardHeader className="p-4 pb-0">
-                    <CardTitle className="text-sm">Need more features?</CardTitle>
-                    <CardDescription>
-                        Check out our Pro solution template.
-                    </CardDescription>
-                </CardHeader>
-                <CardContent className="grid gap-2.5 p-4">
-                    <Link
-                        target="_blank"
-                        href={process.env.NEXT_PUBLIC_PRO_SITE_URL ?? '#'}
-                        className={cn(
-                            buttonVariants({ size: 'sm' }),
-                            'w-full bg-sidebar-primary text-sidebar-primary-foreground shadow-none'
-                        )}
-                    >
-                        Pro version <SquareArrowOutUpRight />
-                    </Link>
-                </CardContent>
-            </form>
-        </Card>
+        <SidebarMenu className="group-data-[collapsible=icon]:hidden">
+            <SidebarMenuItem>
+                <Card className="shadow-none">
+                    <form>
+                        <CardHeader className="p-4 pb-0">
+                            <CardTitle className="text-sm">Need more features?</CardTitle>
+                            <CardDescription>
+                                Check out our Pro solution template.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="grid gap-2.5 p-4">
+                            <Link
+                                target="_blank"
+                                href={process.env.NEXT_PUBLIC_PRO_SITE_URL ?? '#'}
+                                className={cn(
+                                    buttonVariants({ size: 'sm' }),
+                                    'w-full bg-sidebar-primary text-sidebar-primary-foreground shadow-none'
+                                )}
+                            >
+                                Pro version <SquareArrowOutUpRight />
+                            </Link>
+                        </CardContent>
+                    </form>
+                </Card>
+            </SidebarMenuItem>
+        </SidebarMenu>
     )
 }
